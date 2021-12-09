c5957f725ea0d0c8dcddfcf37da306057b24aec4
GameOn456 <95841621+GameOn456@users.noreply.github.com>
2021-12-09 11:06:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetNumber = 7;
+            int guessCount = 0;
+            Console.Write("Guess The Secret Number - ");
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            while (guess != targetNumber)
+            {
+                Console.Write("Guess The Secret Number - ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                guessCount++;
+            }
+
+            Console.WriteLine("You Got It!");
+            Console.WriteLine("Attempts - " + guessCount);
+        }
+    }
+}
